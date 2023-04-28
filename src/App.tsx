13db0f01046301fca9177a9608547968422aa45d
Maliks223 <malik.ekkawi.dev@gmail.com>
2023-04-28 20:35:23 +0300

    added variables & mixins / created pages with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/dashboard";
+import Login from "./Pages/Login/login";
+import Register from "./Pages/Register/register";
+import Navbar from "./Components/Navbar/navbar";
 
 function App() {
-  return <div>react app</div>;
+  return (
+    <>
+      <Router>
+      <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
