4e71c55d68eb8c511920d7c5cac5cd325d08bcd3
Maliks223 <malik.ekkawi.dev@gmail.com>
2023-05-03 10:28:06 +0300

    Added loginPage functionality

--- src/Pages/Login/login.tsx
+++ src/Pages/Login/login.tsx
@@ -1,51 +1,85 @@
-import { useState, ChangeEvent } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import TextInput from "../../Components/TextInput/textInput";
 import "./login.scss";
 import Button from "../../Components/Button/button";
+import axios from "axios";
 
-interface FormData {
+const API_URL = "http://localhost:5000/api/users/login";
+
+interface LoginFormData {
   email: string;
   password: string;
 }
 
-const Login = () => {
-  const [loginFormData, setLoginFormData] = useState<FormData>({
+interface LoginResponse {
+  success: boolean;
+  message: string;
+  token: string;
+}
+
+interface ApiError {
+  message: string;
+  errors?: { [key: string]: string[] };
+}
+
+function Login() {
+  const [formData, setFormData] = useState<LoginFormData>({
     email: "",
     password: "",
   });
+  const [error, setError] = useState<ApiError | null>(null);
 
-  const { email, password } = loginFormData;
+  const navigate = useNavigate()
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const fieldName = e.target.name;
-    const fieldValue = e.target.value;
-    setLoginFormData((prevLoginFormData) => ({
-      ...prevLoginFormData,
-      [fieldName]: fieldValue,
-    }));
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+    setError(null);
   };
-  const onSubmit = () => {
-    // e.preventDefault( )
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post<LoginResponse>(API_URL, formData);
+      localStorage.setItem("token", response.data.token);
+      setError(null);
+      navigate("/");
+      setFormData({
+        email: "",
+        password: "",
+      });
+    } catch (error: any) {
+      setError(error.response.data);
+    }
   };
 
   return (
     <div className="login-page-container">
       <h1 className="login-page-title">login</h1>
-      <form onSubmit={onSubmit} className="login-page-form">
+      <form onSubmit={handleSubmit} className="login-page-form">
+        {error && (
+          <div className="error-notice">
+            <div className="oaerror danger">
+              <strong>Error</strong>- {error.message}
+            </div>
+          </div>
+        )}
         <TextInput
           type="email"
           name="email"
-          onChange={onChange}
-          value={email}
+          onChange={handleChange}
+          value={formData.email}
           placeHolder="Email"
           id="email"
         />
         <TextInput
           type="password"
           name="password"
-          onChange={onChange}
-          value={password}
+          onChange={handleChange}
+          value={formData.password}
           placeHolder="Password"
           id="Password"
         />
@@ -59,6 +93,6 @@ const Login = () => {
       </form>
     </div>
   );
-};
+}
 
 export default Login;
